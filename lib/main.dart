a0d0482541d79f5dfbd0aa71949e745c89843b33
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2024-09-13 11:12:22 +0600

    Hi

--- lib/main.dart
+++ lib/main.dart
@@ -67,95 +67,31 @@ class HomeActivity extends StatelessWidget {
         title: Text("Home"),
         centerTitle: false,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        Activity_1(" Home to Activity_1")),
-              );
-            },
-            child: Text("Go Activity_1"),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        Activity_2("Home to Activity_2")),
-              );
-            },
-            child: Text("Go Activity_2"),
-          ),
-        ],
-      ),
+     body: Center(
+     //     child:Card(
+     //       elevation: 50,
+     //       color: Colors.orangeAccent,
+     //       shadowColor: Colors.orangeAccent,
+     //       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+     //       child: SizedBox(
+     //         height: 200,
+     //         width: 200,
+     //         child: Center(
+     //           child: Text("This is card"),
+     //         ),
+     //       ),
+     // ),
+     //   child: CircularProgressIndicator(
+     //     color: Colors.blueAccent,
+     //   ),
+       child: LinearProgressIndicator(
+         minHeight: 4,
+         backgroundColor: Colors.blueAccent,
+         borderRadius: BorderRadius.circular(20),
+       ),
+
+     ),
     );
   }
 }
 
-class Activity_1 extends StatelessWidget {
-  String msg;
-
-  Activity_1(this.msg, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-        //centerTitle: false,
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        Activity_2(" Acitivity_1 to Activity_2")),
-              );
-            },
-            child: Text("Go Activity_2")),
-      ),
-    );
-  }
-}
-
-class Activity_2 extends StatelessWidget {
-  String msg;
-
-  Activity_2(this.msg, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-        //centerTitle: false,
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        Activity_1("Acitivity_2 to Activity_1")),
-              );
-            },
-            child: Text("Go Activity_1")),
-      ),
-    );
-  }
-}
