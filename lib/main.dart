4f64f966dd0a509763b4f1429686873515eb0db1
Abu Obayed <115519807+AbuObayed25@users.noreply.github.com>
2024-11-25 14:47:21 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:practice_2/app.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:practice_2/home_screen.dart';
 
 void main() {
   runApp(
     DevicePreview(
         enabled: true,
-        builder: (context) => const CounterApp()
+        builder: (context) => const TaskManagerApp()
     ),
   ); // Wrap your app
-}
-
-class CounterApp extends StatelessWidget {
-  const CounterApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
 }
